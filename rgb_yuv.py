7fb1787ba9a7f49ceff0b879f7b0c3ffdbd63be2
Pablo Fernández <pablo.fernandez10@estudiant.upf.edu>
2023-11-01 19:16:04 +0100

    finished

--- rgb_yuv.py
+++ rgb_yuv.py
@@ -1,6 +1,9 @@
 import numpy as np
 import ffmpeg
 import cv2
+import os
+import scipy
+from numpy import r_
 
 
 class Practice1:
@@ -49,12 +52,11 @@ class Practice1:
                             self.serpentine(cur_window)
                             count += 1
                 elif opt == '1':
-                    print('Enter the bottom-left pixel coordinates\nMax row:', height-w_size,
+                    print('Enter the top-left pixel coordinates\nMax row:', height-w_size,
                           'Max column:', width-w_size)
                     ver = int(input('Row: '))
                     hor = int(input('Column: '))
                     cur_window = y_values[ver:ver + w_size, hor:hor + w_size]
-                    cur_window = np.array(cur_window).flatten()
                     self.serpentine(cur_window)
                 else:
                     print('Invalid option')
@@ -63,11 +65,40 @@ class Practice1:
                 image_name = 'messi.jpg'
                 print('Compressed b/w', image_name, '\n')
                 self.compress_bw(image_name)
+                print('Original size: ', os.stat(image_name).st_size, 'bytes')
+                print('We can see how the grayscale image was heavily compressed\n'
+                      'and the file size became 20 times less! Therefore,\n'
+                      'the image quality is much worse. There are big\n'
+                      'areas of pixels with the same color, and it looks really unnatural')
 
             elif exercise == '5':
                 print('Run-length encoding')
                 bit_str = input('Enter a string of bits: ')
                 self.run_length(bit_str)
+
+            elif exercise == '6':
+                print('Performing DCT coding and decoding to paul_robert.jpg\n'
+                      'dct_coeff_paul_robert.jpg stores the DCT coefficients\n'
+                      'output_dct_paul_robert.jpg stores the image after coding and decoding.')
+                im = cv2.imread("paul_robert.jpg")
+                imsize = im.shape
+                dct = np.zeros(imsize)
+                converter = DCT()
+                for i in r_[:imsize[0]:8]:
+                    for j in r_[:imsize[1]:8]:
+                        cur_block = im[i:(i + 8), j:(j + 8)]
+                        dct[i:(i + 8), j:(j + 8)] = converter.dct2(cur_block)
+                cv2.imwrite('dct_coeff_paul_robert.jpg', dct)
+
+                # Threshold of dct coefficients for a greater compression
+                thresh = 0.012
+                dct_thresh = dct * (abs(dct) > (thresh * np.max(dct)))
+
+                im_dct = np.zeros(imsize)
+                for i in r_[:imsize[0]:8]:
+                    for j in r_[:imsize[1]:8]:
+                        im_dct[i:(i + 8), j:(j + 8)] = converter.idct2(dct_thresh[i:(i + 8), j:(j + 8)])
+                cv2.imwrite('output_dct_paul_robert.jpg', im_dct)
 
             elif exercise.upper() == 'EXIT':
                 print('See you soon!')
@@ -87,6 +118,9 @@ class Practice1:
     def yuv_rgb(yuv):
         matrix = [[1.0, 0.0, 1.13983], [1.0, -0.39465, -0.58060], [1.0, 2.03211, 0.0]]
         rgb = np.matmul(matrix, yuv)
+        rgb[0] = round(rgb[0])
+        rgb[1] = round(rgb[1])
+        rgb[2] = round(rgb[2])
         print('RGB values: ', rgb)
         return rgb
 
@@ -100,12 +134,13 @@ class Practice1:
             .output('output_resized_messi.jpg')
             .run()
         )
-        print('Resizing completed. Open ', output_name, 'to see results.')
+        print('Resizing completed. Open', output_name, 'to see results.')
         return
 
     @staticmethod
-    def serpentine(block):     # Expected 8x8 block
-        print('Original block:', block)
+    def serpentine(block):     # Expected block of size 64
+        print('Original block:\n', block)
+        block = np.array(block).flatten()
         scan = [1, 2, 9, 17, 10, 3, 4, 11, 18, 25, 33, 26, 19, 12, 5, 6, 13, 20, 27, 34, 41, 49, 42, 35, 28, 21, 14,
                 7, 8, 15, 22, 29, 36, 43, 50, 57, 58, 51, 44, 37, 30, 23, 16, 24, 31, 38, 45, 52, 59, 60, 53, 46, 39,
                 32, 40, 47, 54, 61, 62, 55, 48, 56, 63, 64]     # Coefficients order
@@ -113,6 +148,7 @@ class Practice1:
         scanned_block = [[0 for _ in range(8)] for _ in range(8)]
         i = 0
         j = 0
+        print('Scanned block:')
         for el in scan:
             scanned_block[j][i] = block[el-1]
             if i == 7:
@@ -120,8 +156,7 @@ class Practice1:
                 j += 1
             else:
                 i += 1
-        print('Scanned block: ', scanned_block)
-
+        print(scanned_block)
         return
 
     @staticmethod
@@ -136,24 +171,38 @@ class Practice1:
         img = cv2.imread('output_bw_messi.jpg')
         compression_params = [cv2.IMWRITE_JPEG_QUALITY, 1]
         cv2.imwrite('output_compressed_bw_messi.jpg', img, compression_params)
-        # We can see how the grayscale image was heavily compressed,
-        # and the image quality is much worse. Pixels in the same area
-        # have now the same values, and it looks really unnatural
+        print('Compressed size: ', os.stat('output_compressed_bw_messi.jpg').st_size, 'bytes')
         return
 
     @staticmethod
     def run_length(bits):
-        encoded = []
+        res = []
         count = 1
         for i in range(1, len(bits)):
+            if bits[i] != '0' and bits[i] != '1':
+                print('Do not cheat, those are not bits!!')
+                return
             if bits[i] == bits[i - 1]:
                 count += 1
             else:
-                encoded.append((bits[i - 1], count))
+                res.append((bits[i - 1], count))
                 count = 1
-        encoded.append((bits[-1], count))
-        print(encoded)
+        res.append((bits[-1], count))
+        print(res)
         return
+
+
+class DCT:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def dct2(im_block):
+        return scipy.fftpack.dct(scipy.fftpack.dct(im_block, axis=0, norm='ortho'), axis=1, norm='ortho')
+
+    @staticmethod
+    def idct2(im_block):
+        return scipy.fftpack.idct(scipy.fftpack.idct(im_block, axis=0, norm='ortho'), axis=1, norm='ortho')
 
 
 if __name__ == '__main__':
