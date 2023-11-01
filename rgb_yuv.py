fd9c0eff90189a83e1b486248f3097a640ffa9fb
Pablo Fernández <pablo.fernandez10@estudiant.upf.edu>
2023-11-01 13:19:35 +0100

    task 6 left

--- rgb_yuv.py
+++ rgb_yuv.py
@@ -1,7 +1,7 @@
 import numpy as np
 import ffmpeg
 import cv2
-import imageio as iio
+
 
 class Practice1:
     def __init__(self):
@@ -28,41 +28,70 @@ class Practice1:
                 self.resize_img(image_name)
             elif exercise == '3':
                 print('Serpentine scan\n')
+                opt = input('Press 1 for scanning a single block, 0 for the whole image\n')
                 filepath = 'barça.jpg'
                 w_size = 8
                 # The width and height of this image are multiples of 8,
                 # which will make the scanning easier
                 img = cv2.imread(filepath)
-                width = img.shape[0]
-                height = img.shape[1]
+                width = img.shape[1]
+                height = img.shape[0]
                 yuv_img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-                y_values = yuv_img[:, :, 0]
-                for r in range(0, height-w_size, w_size):
-                    for c in range(0, width-w_size, w_size):
-                        cur_window = y_values[r:r+w_size, c:c+w_size]
-                        cur_window = np.array(cur_window).flatten()
-                        self.serpentine(cur_window)
-                        print(r, c)
+                y_values = yuv_img[:, :, 0]     # We will scan the Y values
+
+                if opt == '0':
+                    count = 1
+                    for r in range(0, height, w_size):
+                        for c in range(0, width, w_size):
+                            cur_window = y_values[r:r+w_size, c:c+w_size]
+                            cur_window = np.array(cur_window).flatten()
+                            print('Block ', count)
+                            self.serpentine(cur_window)
+                            count += 1
+                elif opt == '1':
+                    print('Enter the bottom-left pixel coordinates\nMax row:', height-w_size,
+                          'Max column:', width-w_size)
+                    ver = int(input('Row: '))
+                    hor = int(input('Column: '))
+                    cur_window = y_values[ver:ver + w_size, hor:hor + w_size]
+                    cur_window = np.array(cur_window).flatten()
+                    self.serpentine(cur_window)
+                else:
+                    print('Invalid option')
+
+            elif exercise == '4':
+                image_name = 'messi.jpg'
+                print('Compressed b/w', image_name, '\n')
+                self.compress_bw(image_name)
+
+            elif exercise == '5':
+                print('Run-length encoding')
+                bit_str = input('Enter a string of bits: ')
+                self.run_length(bit_str)
+
             elif exercise.upper() == 'EXIT':
                 print('See you soon!')
                 return
             else:
                 print('Invalid input, please try again.')
 
-    def rgb_yuv(self, rgb):
+    @staticmethod
+    def rgb_yuv(rgb):
         matrix = [[0.299, 0.587, 0.114], [-0.14713, -0.28886, 0.436], [0.615, -0.51499, -0.10001]]
         yuv = np.matmul(matrix, rgb)
         print('YUV values: ', yuv)
         return yuv
     # Reference: https://www.cs.sfu.ca/mmbook/programming_assignments/additional_notes/rgb_yuv_note/RGB-YUV.pdf
 
-    def yuv_rgb(self, yuv):
+    @staticmethod
+    def yuv_rgb(yuv):
         matrix = [[1.0, 0.0, 1.13983], [1.0, -0.39465, -0.58060], [1.0, 2.03211, 0.0]]
         rgb = np.matmul(matrix, yuv)
         print('RGB values: ', rgb)
         return rgb
 
-    def resize_img(self, img):
+    @staticmethod
+    def resize_img(img):
         output_name = 'output_resized_messi.jpg'
         (
             ffmpeg
@@ -74,29 +103,56 @@ class Practice1:
         print('Resizing completed. Open ', output_name, 'to see results.')
         return
 
-    def serpentine(self, block):     # Expected 8x8 block
+    @staticmethod
+    def serpentine(block):     # Expected 8x8 block
+        print('Original block:', block)
         scan = [1, 2, 9, 17, 10, 3, 4, 11, 18, 25, 33, 26, 19, 12, 5, 6, 13, 20, 27, 34, 41, 49, 42, 35, 28, 21, 14,
                 7, 8, 15, 22, 29, 36, 43, 50, 57, 58, 51, 44, 37, 30, 23, 16, 24, 31, 38, 45, 52, 59, 60, 53, 46, 39,
                 32, 40, 47, 54, 61, 62, 55, 48, 56, 63, 64]     # Coefficients order
-        res_block = block
+
+        scanned_block = [[0 for _ in range(8)] for _ in range(8)]
+        i = 0
+        j = 0
         for el in scan:
-            print(block[el-1])
+            scanned_block[j][i] = block[el-1]
+            if i == 7:
+                i = 0
+                j += 1
+            else:
+                i += 1
+        print('Scanned block: ', scanned_block)
+
         return
 
-    def compress_bw(self, img):
+    @staticmethod
+    def compress_bw(img):
         (
             ffmpeg
             .input(img)
-            .output('output_compressed_messi.jpg', **{'qscale:v=100'})
-            .run()
-        )
-        (
-            ffmpeg
-            .input('output_compressed_messi.jpg')
             .filter('format', 'gray')
-            .output('output_compressed_bw_messi.jpg')
+            .output('output_bw_messi.jpg')
             .run()
         )
+        img = cv2.imread('output_bw_messi.jpg')
+        compression_params = [cv2.IMWRITE_JPEG_QUALITY, 1]
+        cv2.imwrite('output_compressed_bw_messi.jpg', img, compression_params)
+        # We can see how the grayscale image was heavily compressed,
+        # and the image quality is much worse. Pixels in the same area
+        # have now the same values, and it looks really unnatural
+        return
+
+    @staticmethod
+    def run_length(bits):
+        encoded = []
+        count = 1
+        for i in range(1, len(bits)):
+            if bits[i] == bits[i - 1]:
+                count += 1
+            else:
+                encoded.append((bits[i - 1], count))
+                count = 1
+        encoded.append((bits[-1], count))
+        print(encoded)
         return
 
 
@@ -105,7 +161,3 @@ if __name__ == '__main__':
     practice = Practice1()
 
     practice.launch()
-
-    # Task 4
-    # image_name = 'messi.jpg'
-    # compress_bw(image_name)
